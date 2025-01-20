4b0dc47acb4593b5788771c052e252e34cfe55e0
Suphakit P. <me@suphakit.net>
2025-01-21 06:59:29 +0700

    feat<script>: added update function

---
+++ list_update/update-funtion.js
@@ -0,0 +1,5 @@
+const all_list_update_button_function = () => {
+  console.log(document.querySelector("#kitsuUserNameInput").value);
+  console.log(document.querySelector("#malUserNameInput").value);
+  console.log(document.querySelector("#aniListUserNameInput").value);
+};
